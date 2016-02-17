7827e510e2deddad713c99087ee8407142fb4356
Vinicius Chiele <vinicius.chiele@gmail.com>
2016-02-17 20:07:26 +0000

    Adds unit tests

---
+++ src/ScryptTests/ScryptEncoderTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Scrypt;
+
+namespace ScryptTests
+{
+    [TestClass]
+    public class ScryptEncoderTest
+    {
+        [TestMethod]
+        public void TestEncode()
+        {
+            var encoder = new ScryptEncoder();
+            var hashedPassword = encoder.Encode("MyPassword");
+            Assert.IsTrue(encoder.Compare("MyPassword", hashedPassword));
+            Assert.IsFalse(encoder.Compare("WrongPassword", hashedPassword));
+        }
+    }
+}
