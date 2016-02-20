9f10541505d2d40569232e20fcc928a7efac33ac
Vinicius Chiele <vinicius.chiele@gmail.com>
2016-02-20 00:27:06 +0000

    Redo unit tests with xUnit

--- src/Scrypt.Tests/Properties/AssemblyInfo.cs
+++ src/Scrypt.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ScryptTests")]
+[assembly: AssemblyTitle("Scrypt.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ScryptTests")]
+[assembly: AssemblyProduct("Scrypt.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b07830a7-e666-46f3-9f30-344bb8045e10")]
+[assembly: Guid("4e697143-b8cb-41d9-892c-c1ce2571f114")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ src/Scrypt.Tests/ScryptEncoderTests.cs
@@ -0,0 +1,17 @@
+﻿using Scrypt;
+using Xunit;
+
+namespace Scrypt.Tests
+{
+    public class ScryptEncoderTests
+    {
+        [Fact]
+        public void TestEncode()
+        {
+            var encoder = new ScryptEncoder();
+            var hashedPassword = encoder.Encode("MyPassword");
+            Assert.True(encoder.Compare("MyPassword", hashedPassword));
+            Assert.False(encoder.Compare("WrongPassword", hashedPassword));
+        }
+    }
+}
